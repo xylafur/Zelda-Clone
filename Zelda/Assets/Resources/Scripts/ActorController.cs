f5361e28f48c47342f43f87c79fdeaa168ae71bc
Kesley Richardson <jkesleyrichardson@gmail.com>
2016-09-21 16:25:30 -0500

    Got Link moving again :(

---
+++ Zelda/Assets/Resources/Scripts/ActorController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(SpriteRenderer))]
+public class ActorController : MonoBehaviour {
+
+    public Rigidbody2D rb;
+    public Collider2D col;
+    public SpriteRenderer sr;
+
+    public float speed = 1.0f;
+    public string direction = "up";
+    public
+	void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<Collider2D>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+	void Update () {
+	    
+	}
+    void FixedUpdate()
+    {
+        if(speed > 0 && gameObject.tag != "Player")
+        {
+            switch (direction)
+            {
+                case "up":
+                    rb.velocity = new Vector2(0, speed);
+                    break;
+                case "down":
+                    rb.velocity = new Vector2(0, -speed);
+                    break;
+                case "left":
+                    rb.velocity = new Vector2(-speed, 0);
+                    break;
+                case "right":
+                    rb.velocity = new Vector2(speed, 0);
+                    break;
+            }
+        }
+    }
+}
---
+++ Zelda/Assets/Resources/Scripts/LinkController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(ActorController))]
+public class LinkController : MonoBehaviour {
+
+    private ActorController ac;
+    private float hAxis, vAxis;
+    private bool horiz = false, vert = false;
+	void Awake()
+    {
+        ac = GetComponent<ActorController>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        CheckInput();
+        
+	}
+    void FixedUpdate()
+    {
+        ac.rb.velocity = new Vector2(hAxis, vAxis) * ac.speed;
+    }
+
+    void CheckInput()
+    {
+        if (Mathf.Abs(Input.GetAxis("Horizontal")) > 0.0f)
+        {
+            hAxis = Input.GetAxis("Horizontal");
+            vAxis = 0.0f;
+        }
+        else if (Mathf.Abs(Input.GetAxis("Vertical")) > 0.0f)
+        {
+            vAxis = Input.GetAxis("Vertical");
+            hAxis = 0.0f;
+        }
+        else
+        {
+            hAxis = 0.0f;
+            vAxis = 0.0f;
+        }
+    }
+
+}
